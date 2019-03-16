bf8f1f54fcaffe8f31a27b7ffa23480afa6446d0
caitlinpb <caitlinpalmerbright@gmail.com>
2019-03-17 06:18:14 +1000

    Initial Commit

---
+++ Foodtracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foodtracker
+//
+//  Created by Caitlin Palmer-Bright on 17/3/19.
+//  Copyright © 2019 Bitsmith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
