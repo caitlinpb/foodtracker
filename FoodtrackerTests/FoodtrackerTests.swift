621f31a4aa9c4ca8f3cf50a9fa04615cdbef81a4
caitlinpb <caitlin.palmer-bright@tworedkites.com>
2019-03-17 13:05:09 +1000

    add meal class and tests

--- FoodtrackerTests/FoodtrackerTests.swift
+++ FoodtrackerTests/FoodtrackerTests.swift
@@ -33,4 +33,29 @@ class FoodtrackerTests: XCTestCase {
         }
     }
     
+    //MARK: Meal Class Tests
+    
+    // Confirm that the meal initializer returns a Meal object when passed valid parameters.
+    
+    func testMealInitializationSucceeds() {
+        // Zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest positive rating
+        let positiveRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+        
+        // Highest positive rating
+        let negativeRatingMeal = Meal.init(name: "Zero", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        // Empty name string
+        let emptyNameMeal = Meal.init(name: "", photo: nil, rating: 5)
+        XCTAssertNil(emptyNameMeal)
+        
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+    }
 }
